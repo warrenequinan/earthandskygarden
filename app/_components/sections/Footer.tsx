ddfd7f20988a54693f83bfbe0224467708c77d6c
Warren Equinan <warren.pusing.equinan@gmail.com>
2026-06-12 07:13:21 +0800

    Add footer section

---
+++ app/_components/sections/Footer.tsx
@@ -0,0 +1,12 @@
+import CtaBanner from "@/app/_components/ui/CtaBanner";
+
+const Footer = () => {
+  return (
+    <footer>
+      <CtaBanner />
+      <p>Footer here</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ app/_components/ui/CtaBanner.tsx
@@ -0,0 +1,5 @@
+const CtaBanner = () => {
+  return <p>Cta Banner</p>;
+};
+
+export default CtaBanner;
